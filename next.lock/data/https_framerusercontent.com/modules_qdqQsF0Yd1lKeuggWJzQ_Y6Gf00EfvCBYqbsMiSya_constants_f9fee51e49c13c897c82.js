f0857f4fc8b40d34ea3dde311ed922aa1d1c0b5f
soso01 <6125024@gmail.com>
2022-02-04 12:17:34 +0900

    <refactor> next.lock gitignore 삭제

---
+++ next.lock/data/https_framerusercontent.com/modules_qdqQsF0Yd1lKeuggWJzQ_Y6Gf00EfvCBYqbsMiSya_constants_f9fee51e49c13c897c82.js
@@ -0,0 +1,3 @@
+export const TODO_TYPE={ALL:"All",TODO:"Todo",DOING:"Doing",DONE:"Done"};
+export const __FramerMetadata__ = {"exports":{"TODO_TYPE":{"type":"variable","annotations":{"framerContractVersion":"1"}},"__FramerMetadata__":{"type":"variable"}}}
+//# sourceMappingURL=./constants.map
